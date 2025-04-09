d1c7de2ae1d691c3cddb67d6dca893b016af9988
Jinyoung <glitt5384@naver.com>
2025-04-09 11:13:19 +0900

    FEAT: 레디스, 몽고 서비스 추가

--- src/main/java/com/recap/crud/controller/CrudController.java
+++ src/main/java/com/recap/crud/controller/CrudController.java
@@ -1,6 +1,8 @@
 package com.recap.crud.controller;
 
+import com.recap.crud.service.MongoCrudService;
 import com.recap.crud.service.MySQLCrudService;
+import com.recap.crud.service.RedisCrudService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,55 @@ import org.springframework.web.bind.annotation.*;
 public class CrudController {
 
     private final MySQLCrudService mysqlCrudService;
+    private final MongoCrudService mongoCrudService;
+    private final RedisCrudService redisCrudService;
 
     @PostMapping
     public Object postUser(@RequestParam String type, @RequestParam String userId, @RequestParam String name, @RequestParam String password){
-         return mysqlCrudService.createUser(name, userId, password);
+        if (type.equals("mysql")){
+            return mysqlCrudService.createUser(name, userId, password);
+        }
+        else if (type.equals("mongo")) {
+            return mongoCrudService.createUser(name, userId, password);
+        }
+        else{
+            return redisCrudService.createUser(name, userId, password);
+        }
+    }
+    @GetMapping
+    public Object getUser(@RequestParam String type, @RequestParam String id){
+        if (type.equals("mysql")){
+            return mysqlCrudService.readUserById(Integer.parseInt(id));
+        }
+        else if (type.equals("mongo")){
+            return mongoCrudService.readUserById(id);
+        }
+        else {
+            return redisCrudService.readUserById(id);
+        }
+    }
+    @PutMapping
+    public Object putUser(@RequestParam String type, @RequestParam String id, @RequestParam String userId){
+        if (type.equals("mysql")){
+            return mysqlCrudService.updateUserId(Integer.parseInt(id), userId);
+        }
+        else if (type.equals("mongo")){
+            return mongoCrudService.updateUserId(id, userId);
+        }
+        else {
+            return redisCrudService.updateUserId(id, userId);
+        }
+    }
+    @DeleteMapping
+    public Object deleteUser(@RequestParam String type, @RequestParam String id){
+        if (type.equals("mysql")){
+            return mysqlCrudService.deleteUserById(Integer.parseInt(id));
+        }
+        else if (type.equals("mongo")){
+            return mongoCrudService.deleteUserById(id);
+        }
+        else {
+            return redisCrudService.deleteUserById(id);
+        }
     }
 }
