907487ff72f41b1c34a63889eb1045e47d4c286b
Jinyoung <glitt5384@naver.com>
2025-04-11 20:41:21 +0900

    REFACTOR: Mongo 코드 삭제

--- src/main/java/com/recap/domain/repository/MongoUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recap.domain.repository;
-
-import com.recap.domain.entity.MongoUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MongoUserRepository extends MongoRepository<MongoUser, String> {
-}
