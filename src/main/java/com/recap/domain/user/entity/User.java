acbe880f287a523c4d0356e30eebbd59fb763cbe
Jinyoung <glitt5384@naver.com>
2025-05-04 22:13:57 +0900

    FEAT: 사용자 엔티티

--- src/main/java/com/recap/domain/user/entity/User.java
+++ src/main/java/com/recap/domain/user/entity/User.java
@@ -2,13 +2,11 @@ package com.recap.domain.user.entity;
 
 import com.recap.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Builder
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "user")
@@ -17,7 +15,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "user_id", length = 20, nullable = false)
+    @Column(length = 20, nullable = false)
     private String userId;
 
     @Column(name = "password", nullable = false) // length default = 255, 별도 명시 x
