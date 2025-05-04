d1856e21cddc12c0ff983dce1b68be37004d1eb9
Jinyoung <glitt5384@naver.com>
2025-05-04 22:12:45 +0900

    DELETE: 미사용 클래스 제거

--- src/main/java/com/recap/global/repository/RedisUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recap.global.repository;
-
-import com.recap.global.entity.RedisUser;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RedisUserRepository extends CrudRepository<RedisUser, String> {
-}
