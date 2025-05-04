771a7a6e8d3ce959121f4d5954cd590867677a22
Jinyoung <glitt5384@naver.com>
2025-05-04 22:13:31 +0900

    FEAT: 게시글 작성 구현

--- src/main/java/com/recap/domain/post/controller/PostController.java
+++ src/main/java/com/recap/domain/post/controller/PostController.java
@@ -1,13 +1,28 @@
 package com.recap.domain.post.controller;
 
+import com.recap.domain.post.dto.PostRequest;
+import com.recap.domain.post.entity.Post;
+import com.recap.domain.post.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
-    // TODO: 게시글 작성
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Post> postNewPost(@Valid @RequestBody PostRequest postRequest){
+        return ResponseEntity.ok(postService.createPost(postRequest));
+    }
     // TODO: 게시글 삭제
     // TODO: 게시글 수정
     // TODO: 게시글 신고
