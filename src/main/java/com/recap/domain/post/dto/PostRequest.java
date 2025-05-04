f3f8634c06ce94407f6bff4d8db0b43cedd26c04
Jinyoung <glitt5384@naver.com>
2025-05-04 22:13:04 +0900

    FEAT: 게시글 작성 시 받는 Dto

---
+++ src/main/java/com/recap/domain/post/dto/PostRequest.java
@@ -0,0 +1,20 @@
+package com.recap.domain.post.dto;
+
+import com.recap.domain.post.entity.Post;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+
+@Builder
+public record PostRequest (
+    @NotNull @Size(max = 90) String title,
+    @NotNull String content
+){
+    public Post to(){
+        return Post.builder()
+                .title(this.title)
+                .content(this.content)
+                .build();
+    }
+}
